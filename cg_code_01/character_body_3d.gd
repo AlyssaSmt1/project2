ebcb81aca055df2a5ffa94316f7f6896e8e6ece6
Your Name <you@example.com>
2025-06-16 14:29:41 +0200

    Character

--- cg_code_01/character_body_3d.gd
+++ cg_code_01/character_body_3d.gd
@@ -1,27 +1,49 @@
 extends CharacterBody3D
 
-const MOVE_SPEED := 500.0
-const ROTATE_SPEED := 2.5
 
-func _physics_process(delta):
-	var input_vector = Vector2(
-		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-		Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
-	)
+@export var SPEED = 3.0
+@export var JUMP_VELOCITY = 4.5
+@export var camera_pan_speed = 1
+const PAN_SPEED_FACTOR = 0.00001
 
-	if input_vector.length() > 0:
-		# Rotation (Links/Rechts)
-		rotation.y -= input_vector.x * ROTATE_SPEED * delta
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
-		# Vorwärts-/Rückwärtsbewegung (Z-Achse)
-		var forward = transform.basis.z.normalized()
-		var move_velocity = forward * input_vector.y * MOVE_SPEED
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
-		# Behalte vertikale Geschwindigkeit (z. B. Gravitation)
-		velocity.x = move_velocity.x
-		velocity.z = move_velocity.z
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("player_left", "player_right", "player_forward", "player_backwards")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = 0
-		velocity.z = 0
-
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		var mouse_vel = Input.get_last_mouse_velocity()
+		rotation.y -= mouse_vel.x * camera_pan_speed * PAN_SPEED_FACTOR
+		var cam_rig_rot = $CameraRig.rotation.x + mouse_vel.y * camera_pan_speed * PAN_SPEED_FACTOR
+		cam_rig_rot = clamp(cam_rig_rot, -PI/2, -PI/8)
+		$CameraRig.rotation.x = cam_rig_rot
+		
+		
+		if Input.is_action_just_pressed("ui_cancel"):
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	else:
+		if Input.is_action_just_pressed("ui_cancel"):
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		
+		
 	move_and_slide()
